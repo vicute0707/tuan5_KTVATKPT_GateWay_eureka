174b2be5cb109daf635211022cc1684e66e0bcc1
vi0707 <vi0978294041@gmail.com>
2024-03-15 01:34:56 +0700

    tuan5

---
+++ src/test/java/com/javademo/registrydiscovery/RegistryDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javademo.registrydiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
